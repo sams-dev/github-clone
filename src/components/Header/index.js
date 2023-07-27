7f5dcce595a4780d990da761c9b7dbf4d0f7e5df
sams-dev <samuel2905.sms@gmail.com>
2023-07-27 12:02:36 -0300

    Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,24 @@
+
 const Header = () => {
-    return (
-      <h1>header page</h1>
+  return (
+    <header className="container-fluid bg-dark">
+      <div className="container">
+        <div className="row align-items-center">
+          <div>
+            <img src={require("../../assets/logo.png")}/>
+          </div>
+          <div>
+            <ul id="main-menu" className="display-flex justfy-end">
+              <li><a href="#">Features</a></li>
+              <li><a href="#">Business</a></li>
+              <li><a href="#">Explore</a></li>
+              <li><a href="#">Marketplace</a></li>
+              <li><a href="#">Pricing</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
+import Header from './components/Header';
 import './styles/global.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Header />
   </React.StrictMode>
 );
 
