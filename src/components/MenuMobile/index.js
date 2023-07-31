985d07d529544531882888e0ca7669091dac1b98
sams-dev <samuel2905.sms@gmail.com>
2023-07-31 14:52:02 -0300

    MenuMobile component

---
+++ src/components/MenuMobile/index.js
@@ -0,0 +1,9 @@
+const MenuMobile = () => {
+  return (
+    <div id="main-menu-mobile" className="display-flex justify-end">
+      <span className="mdi mdi-menu color-white"></span>
+    </div>
+    );
+};
+
+export default MenuMobile;
