8f99005062afe8ba7f6a7d08bc3c09d31eeb8a1e
sams-dev <samuel2905.sms@gmail.com>
2023-07-31 13:00:09 -0300

    media query - responsive

--- src/components/SectionBottom/index.js
+++ src/components/SectionBottom/index.js
@@ -13,7 +13,7 @@ const SectionBottom = () => {
 
       <div className="Container">
         <div className="row align-items-center">
-          <div className="box-img"></div>
+          <div className="box-img-e"></div>
           <div>
             <h1 className="color-dark">Seus projetos em um só lugar.</h1>
             <p className="color-grey">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining</p>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
 import './styles/global.css';
+import './styles/responsive.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
