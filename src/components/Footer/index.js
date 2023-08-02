0c4d851a49c0c3a3fe46d281d93c41609def0736
sams-dev <samuel2905.sms@gmail.com>
2023-08-01 23:12:08 -0300

    atualização dentro do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,7 @@ const Footer = () => {
       <div className="container">
         <div className="row align-items-center">
           <h1 className="color-white text-align-center">.developed by samsdev</h1>
-         
-            <img id="dsw-img" src={require("../../assets/SDs.png")} />
-          
+            <img id="sd-img" src={require("../../assets/SDs.png")} />
         </div>
       </div>
     </footer>
